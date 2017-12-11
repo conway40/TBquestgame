ec0b142c73c6d9b367b9d763a2a2d15f5058c3aa
conway40 <conway40@mail.nmc.edu>
2017-12-11 01:20:07 -0500

    Add project files.

---
+++ TB_QuestGame/Models/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB_QuestGame
+{
+    public class Animal : Npc, IScavenge
+    {
+        public override int Id { get; set; }
+        public override string Description { get; set; }
+
+        public SurvivorObject ScavengeForObjects(List<SurvivorObject> scavengerObjects)
+        {
+            SurvivorObject foundObject = new SurvivorObject();
+            Random rnd = new Random();
+            if (scavengerObjects.Count > 0)
+            {
+                int scavengerObjectId = rnd.Next(1, scavengerObjects.Count);
+                foundObject = scavengerObjects[scavengerObjectId];
+            }
+
+            return foundObject;
+        }
+    }
+}
